376abe245503e6729fd6ee1faf1007f6875d19a7
esma <esma.ea.aksoy@gmail.com>
2023-11-11 19:41:36 +0300

    first

--- bootstrapt/script.js
+++ bootstrapt/script.js
@@ -1,10 +1,22 @@
-const countShoppingNumber = document.querySelector("#header-shopping")
-const cartShopping = document.querySelectorAll(".icons")
-console.log(cartShopping);
+// const countShoppingNumber = document.querySelector("#header-shopping")
+// const cartShopping = document.querySelectorAll(".icons")
+// console.log(cartShopping);
 
-const sepet = document.querySelector(".header__div--a");
-sepet.addEventListener("click", ()=>{
-    window.open("../checkout/check.html")
+// const sepet = document.querySelector(".header__div--a");
+// sepet.addEventListener("click", ()=>{
+//     window.open("../checkout/check.html")
+// })
+
+
+document.querySelector("main").addEventListener("click",(event)=>{
+    event.preventDefault()
+    if(event.target.classList.contains("fa-cart-shopping")){
+        console.log("hi");
+        
+        document.querySelector(".cart-badge").textContent = Number( document.querySelector(".cart-badge").textContent) +1
+        console.log(document.querySelector(".cart-badge").textContent );
+        
+    }
+
 })
-
 
