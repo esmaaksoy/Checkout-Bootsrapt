fc5f0a4e35a48037a6a0af0d2f8e0d4282d75fb8
esma <esma.ea.aksoy@gmail.com>
2023-11-12 16:03:40 +0300

    price,bookname

--- bootstrapt/script.js
+++ bootstrapt/script.js
@@ -2,8 +2,8 @@ const countShoppingNumber = document.querySelector("#header-shopping")
 const cartShopping = document.querySelectorAll(".icons")
 console.log(cartShopping);
 
-const sepet = document.querySelector(".header__div--a");
-sepet.addEventListener("click", ()=>{
+const cart = document.querySelector(".header__div--a");
+cart.addEventListener("click", ()=>{
     window.open("../check/check.html")
 })
 
@@ -11,12 +11,18 @@ sepet.addEventListener("click", ()=>{
 document.querySelector("main").addEventListener("click",(event)=>{
     event.preventDefault()
     if(event.target.classList.contains("fa-cart-shopping")){
-        document.querySelector(".cart-badge").textContent = Number( document.querySelector(".cart-badge").textContent) +1
-        const imageProduct = event.target.closest(".card").querySelector("img");
-      
-  
-  const imageSrc = imageProduct.getAttribute("src");
-  localStorage.setItem("selectedImageSrc", imageSrc)
+        document.querySelector(".cart-badge").textContent = Number( document.querySelector(".cart-badge").textContent) +1 //"When adding a product to the cart, the value of the cart in the header changes."
+        //Selected 
+        const imageProduct = event.target.closest(".card").querySelector("img"); //Selects the image in the cart when clicked.
+        const imageSrc = imageProduct.getAttribute("src"); //"Gets the src content of the selected image."
+        const bookName = event.target.closest(".card").querySelector(".card-title").textContent
+        const price = event.target.closest(".card").querySelector(".card-text span").textContent;
+        console.log(price);
+
+        //localStorage
+        localStorage.setItem("selectedImageSrc", imageSrc); //"Saves the src of the image to the local storage."
+        localStorage.setItem("selectedPrice",price)
+        localStorage.setItem("selectedBookName", bookName)
 
     }
 
--- check/check.js
+++ check/check.js
@@ -1,3 +1,10 @@
 
 const imageSrc = localStorage.getItem("selectedImageSrc");
 document.querySelector(".image").src = imageSrc
+
+const priceValue = localStorage.getItem("selectedPrice");
+document.querySelector(".productPrice").textContent = priceValue
+
+const booksName = localStorage.getItem("selectedBookName");
+document.querySelector(".booksName").textContent = booksName;
+
