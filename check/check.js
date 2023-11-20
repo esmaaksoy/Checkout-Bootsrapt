0ddaa9e09096c94990ca76898f303291021511ac
esma <esma.ea.aksoy@gmail.com>
2023-11-20 14:13:22 +0300

    deneme

--- check/check.js
+++ check/check.js
@@ -1,21 +1,40 @@
-const imageSrc = localStorage.getItem("selectedImageSrc");
-document.querySelector(".image").src = imageSrc
+const existingProductsJSON = localStorage.getItem('shoppingCart');
+const existingProducts = existingProductsJSON ? JSON.parse(existingProductsJSON) : [];
+document.querySelector(".noProduct").style.display = "none";
 
-const priceValue = localStorage.getItem("selectedPrice");
-document.querySelector(".productPrice").textContent = priceValue
+const productsContainer = document.querySelector(".products");
 
-const booksName = localStorage.getItem("selectedBookName");
-document.querySelector(".booksName").textContent = booksName;
-document.querySelector(".noProduct").style.display="none"
-//............
+existingProducts.forEach((product) => {
+  const productDiv = document.createElement("div");
+  productDiv.classList.add("product", "p-2", "d-flex", "justify-content-around", "border-bottom", "border-0", "border-secondary-subtle", "mb-2");
+
+  productDiv.innerHTML = `
+    <div class="d-flex align-items-center">
+      <img src="${product.imageSrc}" alt="" width="100px" class="me-3 image" />
+      <p class="booksName h3">${product.productName}</p>
+    </div>
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="p-2 border border-1 border-dark rounded-2 p-1">
+        <i class="fa-solid fa-plus fa-2x me-4"></i>
+        <span class="h3 me-4 number">1</span>
+        <i class="fa-solid fa-minus fa-2x"></i>
+      </div>
+    </div>
+    <div class="d-flex align-items-center justify-content-center">
+      <p class="productPrice me-5">${product.productPrice}</p>
+      <i class="fa-solid fa-trash-can fa-2x"></i>
+    </div>
+  `;
+
+  productsContainer.appendChild(productDiv);
+});
+//..............................
 document.querySelector(".products").addEventListener("click", (event)=>{
 if(event.target.classList.contains("fa-plus")){
     document.querySelector(".number").textContent++
 const quantity = Number(document.querySelector(".number").textContent);
 document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
 
-// document.querySelector(".cart").textContent= document.querySelector(".productPrice").textContent
-// document.querySelector(".ship").textContent= document.querySelector(".cart").textContent > 300 ? "00.00" : "50.00"
 totalCalc()
 }else if(event.target.classList.contains("fa-minus")){
     if(document.querySelector(".number").textContent>1){
@@ -48,40 +67,26 @@ document.querySelector(".ship").textContent= document.querySelector(".cart").tex
 document.querySelector(".totalPrice").textContent = (Number(document.querySelector(".cart").textContent) + Number(document.querySelector(".ship").textContent)).toFixed(2)
 }
 
+//........................................................................................
 
+// const imageSrc = localStorage.getItem("selectedImageSrc");
+// document.querySelector(".image").src = imageSrc
 
+// const priceValue = localStorage.getItem("selectedPrice");
+// document.querySelector(".productPrice").textContent = priceValue
 
-//! JSON denemelerim............................................
-
-// //......................................................................................................
-// const shoppingCartData = JSON.parse(localStorage.getItem('shoppingCart')) || [];
+// const booksName = localStorage.getItem("selectedBookName");
+// document.querySelector(".booksName").textContent = booksName;
 // document.querySelector(".noProduct").style.display="none"
 // //............
 // document.querySelector(".products").addEventListener("click", (event)=>{
 // if(event.target.classList.contains("fa-plus")){
 //     document.querySelector(".number").textContent++
-//     const quantity = Number(document.querySelector(".number").textContent);
-//     document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
-// //....................
-// const existingProductIndex = shoppingCartData.findIndex(
-//     (product) => product.productName === booksName
-// );
+// const quantity = Number(document.querySelector(".number").textContent);
+// document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
 
-// if (existingProductIndex !== -1) {
-//     shoppingCartData[existingProductIndex].quantity = quantity;
-// } else {
-//     // Daha önce eklenmemişse yeni ürünü ekle
-//     const productInfo = {
-//         productName: booksName,
-//         productPrice: priceValue,
-//         quantity: quantity,
-//     };
-//     shoppingCartData.push(productInfo);
-// }
-
-
-
-
+// // document.querySelector(".cart").textContent= document.querySelector(".productPrice").textContent
+// // document.querySelector(".ship").textContent= document.querySelector(".cart").textContent > 300 ? "00.00" : "50.00"
 // totalCalc()
 // }else if(event.target.classList.contains("fa-minus")){
 //     if(document.querySelector(".number").textContent>1){
@@ -99,6 +104,7 @@ document.querySelector(".totalPrice").textContent = (Number(document.querySelect
 // }
 // })
 
+// console.log(document.querySelector(".continue"));
 // document.querySelector(".continue").addEventListener("click", ()=>{
 //     window.open("../bootstrapt/index.html")
 // })
@@ -112,6 +118,7 @@ document.querySelector(".totalPrice").textContent = (Number(document.querySelect
 // document.querySelector(".ship").textContent= document.querySelector(".cart").textContent > 300 ? "00.00" : "50.00"
 // document.querySelector(".totalPrice").textContent = (Number(document.querySelector(".cart").textContent) + Number(document.querySelector(".ship").textContent)).toFixed(2)
 // }
+
 
 
 
