bc67b7510948a2d8dfcf308bf5645a07ce64d6f0
esma <esma.ea.aksoy@gmail.com>
2023-11-27 15:50:15 +0300

    style

--- bootstrapt/script.js
+++ bootstrapt/script.js
@@ -1,31 +1,3 @@
-// const countShoppingNumber = document.querySelector("#header-shopping")
-// const cartShopping = document.querySelectorAll(".icons")
-// const cart = document.querySelector(".header__div--a");
-// //new page
-// cart.addEventListener("click", ()=>{
-//     window.open("../check/check.html")
-// })
-// //............................
-// document.querySelector("main").addEventListener("click",(event)=>{
-//     event.preventDefault()
-//     if(event.target.classList.contains("fa-cart-shopping")){
-//         document.querySelector(".cart-badge").textContent = Number( document.querySelector(".cart-badge").textContent) +1 //"When adding a product to the cart, the value of the cart in the header changes."
-//         //Selected 
-//         const imageProduct = event.target.closest(".card").querySelector("img"); //Selects the image in the cart when clicked.
-//         const imageSrc = imageProduct.getAttribute("src"); //"Gets the src content of the selected image."
-//         const bookName = event.target.closest(".card").querySelector(".card-title").textContent
-//         const price = event.target.closest(".card").querySelector(".card-text span").textContent;
-//         console.log(price);
-//         //localStorage
-//         localStorage.setItem("selectedImageSrc", imageSrc); //"Saves the src of the image to the local storage."
-//         localStorage.setItem("selectedPrice",price)
-//         localStorage.setItem("selectedBookName", bookName)
-
-
-//     }
-// })
-
-
 document.addEventListener('DOMContentLoaded', function() {
     const existingProductsJSON = localStorage.getItem('shoppingCart');
     const existingProducts = existingProductsJSON ? JSON.parse(existingProductsJSON) : [];
@@ -35,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
       
     }
 })  
-// //!JSON DENEMELERİM.................
 const countShoppingNumber = document.querySelector("#header-shopping")
 const cartShopping = document.querySelectorAll(".icons")
 const cart = document.querySelector(".header__div--a");
@@ -45,7 +16,6 @@ const cartBadge = document.querySelector(".cart-badge");
 cart.addEventListener("click", ()=>{
     window.open("../check/check.html")
 })
-//............................
 document.querySelector("main").addEventListener("click",(event)=>{
     event.preventDefault()
     if(event.target.classList.contains("fa-cart-shopping")){
@@ -63,10 +33,7 @@ document.querySelector("main").addEventListener("click",(event)=>{
           const existingProducts = existingProductsJSON ? JSON.parse(existingProductsJSON) : [];
           existingProducts.push(productInfo);
           localStorage.setItem('shoppingCart', JSON.stringify(existingProducts));
-      
-      
     }
-
 })
 
 
--- check/check.js
+++ check/check.js
@@ -9,18 +9,18 @@ existingProducts.forEach((product,index) => {
   productDiv.classList.add("product", "p-2", "d-flex", "justify-content-around", "border-bottom", "border-0", "border-secondary-subtle", "mb-2");
 
   productDiv.innerHTML = `
-    <div class="d-flex align-items-center">
+    <div class="d-flex align-items-center"style="width: 500px" >
       <img src="${product.imageSrc}" alt="" width="100px" class="me-3 image" />
       <p class="booksName h3">${product.productName}</p>
     </div>
-    <div class="d-flex justify-content-between align-items-center">
+    <div class="d-flex justify-content-between align-items-center" style="width: 200px" >
       <div class="p-2 border border-1 border-dark rounded-2 p-1">
         <i class="fa-solid fa-plus fa-2x me-4"></i>
         <span class="h3 me-4 number" data-number="${index}">1</span>
         <i class="fa-solid fa-minus fa-2x"></i>
       </div>
     </div>
-    <div class="d-flex align-items-center justify-content-center">
+    <div class="d-flex align-items-center justify-content-center"style="width: 200px" >
       <p class="productPrice me-5" >${product.productPrice}</p>
       <i class="fa-solid fa-trash-can fa-2x"></i>
     </div>
@@ -88,30 +88,32 @@ document.querySelector(".totalPrice").textContent = (Number(document.querySelect
 // document.querySelector(".booksName").textContent = booksName;
 // document.querySelector(".noProduct").style.display="none"
 // //............
-// document.querySelector(".products").addEventListener("click", (event)=>{
-// if(event.target.classList.contains("fa-plus")){
-//     document.querySelector(".number").textContent++
-// const quantity = Number(document.querySelector(".number").textContent);
-// document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
+document.querySelector(".products").addEventListener("click", (event)=>{
+if(event.target.classList.contains("fa-plus")){
+  // const number =   document.querySelector(".number").textContent++
+  // event.target.closest("number").textcontent++
+  
+const quantity = Number(document.querySelector(".number").textContent);
+document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
 
-// // document.querySelector(".cart").textContent= document.querySelector(".productPrice").textContent
-// // document.querySelector(".ship").textContent= document.querySelector(".cart").textContent > 300 ? "00.00" : "50.00"
-// totalCalc()
-// }else if(event.target.classList.contains("fa-minus")){
-//     if(document.querySelector(".number").textContent>1){
-//         document.querySelector(".number").textContent--
-//         const quantity = Number(document.querySelector(".number").textContent);
-// document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
-//     }document.querySelector(".cart").textContent= document.querySelector(".productPrice").textContent;
-//     totalCalc()
-// }else if(event.target.classList.contains("fa-trash-can")){
-//     document.querySelector(".products").style.display="none"
-//     document.querySelector(".noProduct").style.display="block"
-//     document.querySelector(".cart").textContent = "0.00"
-//     document.querySelector(".ship").textContent = "0.00"
-//     document.querySelector(".totalPrice").textContent= "0.00"
-// }
-// })
+// document.querySelector(".cart").textContent= document.querySelector(".productPrice").textContent
+// document.querySelector(".ship").textContent= document.querySelector(".cart").textContent > 300 ? "00.00" : "50.00"
+totalCalc()
+}else if(event.target.classList.contains("fa-minus")){
+    if(document.querySelector(".number").textContent>1){
+        document.querySelector(".number").textContent--
+        const quantity = Number(document.querySelector(".number").textContent);
+document.querySelector(".productPrice").textContent = (quantity * priceValue).toFixed(2); 
+    }document.querySelector(".cart").textContent= document.querySelector(".productPrice").textContent;
+    totalCalc()
+}else if(event.target.classList.contains("fa-trash-can")){
+    document.querySelector(".products").style.display="none"
+    document.querySelector(".noProduct").style.display="block"
+    document.querySelector(".cart").textContent = "0.00"
+    document.querySelector(".ship").textContent = "0.00"
+    document.querySelector(".totalPrice").textContent= "0.00"
+}
+})
 
 // console.log(document.querySelector(".continue"));
 // document.querySelector(".continue").addEventListener("click", ()=>{
